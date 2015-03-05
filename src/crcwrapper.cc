cf2b85733cf51f5af5f53c025b4005419714d7f5
yejingfu <yejingfu@hotmail.com>
2015-03-05 16:20:01 +0800

    crc serial API wrapping

--- src/crcwrapper.cc
+++ src/crcwrapper.cc
@@ -7,6 +7,96 @@
 
 using namespace v8;
 
+NAN_METHOD(CRC16_t10dif_01) {
+  NanScope();
+  if (args.Length() != 3) {
+    NanThrowTypeError("Invalid arguments");
+    NanReturnUndefined();
+  }
+  UINT16 init_crc = (UINT16)args[0]->Int32Value();
+  Local<Object> bufObj = args[1]->ToObject();
+  unsigned char *buf = (unsigned char*)(node::Buffer::Data(bufObj));
+  int len = node::Buffer::Length(bufObj);
+  UINT16 crc_value = crc16_t10dif_01(init_crc, buf, len);
+  Local<Number> result = NanNew(crc_value);
+  NanReturnValue(result);
+}
+
+NAN_METHOD(CRC32_ieee_01) {
+  NanScope();
+  if (args.Length() != 3) {
+    NanThrowTypeError("Invalid arguments");
+    NanReturnUndefined();
+  }
+  UINT16 init_crc = (UINT16)args[0]->Int32Value();
+  Local<Object> bufObj = args[1]->ToObject();
+  unsigned char *buf = (unsigned char*)(node::Buffer::Data(bufObj));
+  int len = node::Buffer::Length(bufObj);
+  UINT16 crc_value = crc32_ieee_01(init_crc, buf, len);
+  Local<Number> result = NanNew(crc_value);
+  NanReturnValue(result);
+}
+
+NAN_METHOD(CRC32_iscsi_simple) {
+  NanScope();
+  if (args.Length() != 3) {
+    NanThrowTypeError("Invalid arguments");
+    NanReturnUndefined();
+  }
+  UINT16 init_crc = (UINT16)args[0]->Int32Value();
+  Local<Object> bufObj = args[1]->ToObject();
+  unsigned char *buf = (unsigned char*)(node::Buffer::Data(bufObj));
+  int len = node::Buffer::Length(bufObj);
+  UINT16 crc_value = crc32_iscsi_simple(buf, len, init_crc);
+  Local<Number> result = NanNew(crc_value);
+  NanReturnValue(result);
+}
+
+NAN_METHOD(CRC32_iscsi_baseline) {
+  NanScope();
+  if (args.Length() != 3) {
+    NanThrowTypeError("Invalid arguments");
+    NanReturnUndefined();
+  }
+  UINT16 init_crc = (UINT16)args[0]->Int32Value();
+  Local<Object> bufObj = args[1]->ToObject();
+  unsigned char *buf = (unsigned char*)(node::Buffer::Data(bufObj));
+  int len = node::Buffer::Length(bufObj);
+  UINT16 crc_value = crc32_iscsi_baseline(buf, len, init_crc);
+  Local<Number> result = NanNew(crc_value);
+  NanReturnValue(result);
+}
+
+NAN_METHOD(CRC32_iscsi_00) {
+  NanScope();
+  if (args.Length() != 3) {
+    NanThrowTypeError("Invalid arguments");
+    NanReturnUndefined();
+  }
+  UINT16 init_crc = (UINT16)args[0]->Int32Value();
+  Local<Object> bufObj = args[1]->ToObject();
+  unsigned char *buf = (unsigned char*)(node::Buffer::Data(bufObj));
+  int len = node::Buffer::Length(bufObj);
+  UINT16 crc_value = crc32_iscsi_00(buf, len, init_crc);
+  Local<Number> result = NanNew(crc_value);
+  NanReturnValue(result);
+}
+
+NAN_METHOD(CRC32_iscsi_01) {
+  NanScope();
+  if (args.Length() != 3) {
+    NanThrowTypeError("Invalid arguments");
+    NanReturnUndefined();
+  }
+  UINT16 init_crc = (UINT16)args[0]->Int32Value();
+  Local<Object> bufObj = args[1]->ToObject();
+  unsigned char *buf = (unsigned char*)(node::Buffer::Data(bufObj));
+  int len = node::Buffer::Length(bufObj);
+  UINT16 crc_value = crc32_iscsi_01(buf, len, init_crc);
+  Local<Number> result = NanNew(crc_value);
+  NanReturnValue(result);
+}
+
 NAN_METHOD(CRC16_t10dif_by4) {
   NanScope();
   if (args.Length() != 3) {
@@ -38,7 +128,13 @@ NAN_METHOD(CRC32_ieee_by4) {
 }
 
 void exportCRCComponent(v8::Handle<v8::Object>& exports) {
-printf("Jingfu:exportCRCComponent\n");
+  printf("Jingfu:exportCRCComponent\n");
+  exports->Set(NanNew("crc16_t10dif_01"), NanNew<FunctionTemplate>(CRC16_t10dif_01)->GetFunction());
+  exports->Set(NanNew("crc32_ieee_01"), NanNew<FunctionTemplate>(CRC32_ieee_01)->GetFunction());
+  exports->Set(NanNew("crc32_iscsi_simple"), NanNew<FunctionTemplate>(CRC32_iscsi_simple)->GetFunction());
+  exports->Set(NanNew("crc32_iscsi_baseline"), NanNew<FunctionTemplate>(CRC32_iscsi_baseline)->GetFunction());
+  exports->Set(NanNew("crc32_iscsi_00"), NanNew<FunctionTemplate>(CRC32_iscsi_00)->GetFunction());
+  exports->Set(NanNew("crc32_iscsi_01"), NanNew<FunctionTemplate>(CRC32_iscsi_01)->GetFunction());
   exports->Set(NanNew("crc16_t10dif_by4"), NanNew<FunctionTemplate>(CRC16_t10dif_by4)->GetFunction());
   exports->Set(NanNew("crc32_ieee_by4"), NanNew<FunctionTemplate>(CRC32_ieee_by4)->GetFunction());
 }
--- test/crc_test.js
+++ test/crc_test.js
@@ -10,10 +10,30 @@ var buf = new Buffer(BUF_SIZE);
 for (var i = 0; i < BUF_SIZE; i++) {
   buf[i] = 0;
 }
+var crc0 = isal.crc16_t10dif_01(SEED, buf, BUF_SIZE);
+console.log('crc16_t10dif_01(0): ' + crc0);
+
 var crc1 = isal.crc16_t10dif_by4(SEED, buf, BUF_SIZE);
 console.log('crc16_t10dif_by4(0): ' + crc1);
+
 var crc2 = isal.crc32_ieee_by4(SEED, buf, BUF_SIZE);
 console.log('crc32_ieee_by4(0): ' + crc2);
+
+var crc3 = isal.crc32_ieee_01(SEED, buf, BUF_SIZE);
+console.log('crc32_ieee_01(0): ' + crc3);
+
+var crc4 = isal.crc32_iscsi_simple(SEED, buf, BUF_SIZE);
+console.log('crc32_iscsi_simple(0): ' + crc4);
+
+var crc5 = isal.crc32_iscsi_baseline(SEED, buf, BUF_SIZE);
+console.log('crc32_iscsi_baseline(0): ' + crc5);
+
+var crc6 = isal.crc32_iscsi_00(SEED, buf, BUF_SIZE);
+console.log('crc32_iscsi_00(0): ' + crc6);
+
+var crc7 = isal.crc32_iscsi_01(SEED, buf, BUF_SIZE);
+console.log('crc32_iscsi_01(0): ' + crc7);
+
 
 console.log('======Test CRC end');
 
