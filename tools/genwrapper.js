6c080663d52e301a1f469160fb3c11ca36537279
yejingfu <yejingfu@hotmail.com>
2015-03-13 10:57:27 +0800

    automatical tool to generate wrapper of ISA-L API

--- tools/genwrapper.js
+++ tools/genwrapper.js
@@ -65,14 +65,24 @@ var excluded = [
   'sha512_sse4',
   'sha512_sb_mgr_init_sse4',
   'sha512_sb_mgr_submit_sse4',
-  'sha512_sb_mgr_flush_sse4'
+  'sha512_sb_mgr_flush_sse4',
+
+  'mem_cpy_sse',
+  'mem_cpy_avx'
 ];
+
+function filterFunction(func) {
+  for (var i = 0; i < excluded.length; i++) {
+    if (excluded[i] === func) return true;
+  }
+  return false;
+}
 
 function main() {
   var fullpath;
   var generators = [];
   for (var i = 0, len = headerfiles.length; i < len; i++) {
-    if (i !== 0) continue;
+    if (i !== 1) continue;
     var headername = headerfiles[i];
     fullpath = path.join('include', headername) + '.h.func';
     var gen = new Generator(headername, fullpath);
@@ -90,7 +100,7 @@ var Generator = function(headername, filepath) {
 Generator.prototype = {
   generate: function() {
     var self = this;
-    console.log('Handling ' + self.filePath);
+    console.log('====Handling ' + self.filePath);
     fs.exists(self.filePath, function(exists) {
       if (!exists) return console.log('The file does not exists: ' + self.filePath);
       fs.readFile(self.filePath, function(err, data) {
@@ -129,6 +139,7 @@ Generator.prototype = {
     var pos, i, len;
     for (i = 0, len = lines.length; i < len; i++) {
       line = lines[i];
+      if (line.length < 5) continue;
       //console.log('line ' + i + ': ' + line);
       tokens = line.split(' ');
       if (tokens.length < 5) {
@@ -137,22 +148,47 @@ Generator.prototype = {
       }
       funcRet = [];
       funcArgs = [];
+      funcName = undefined;
       pos = 0;
-      funcRet.push(tokens[pos++]);
-      if (tokens[pos] === '*') {
-        funcRet.push(tokens[pos++]);
+      while (pos < tokens.length - 1) {
+        if (!funcName && tokens[pos+1] === '(') {
+          funcName = tokens[pos++];
+          pos++;
+          continue;
+        }
+        if (!funcName) {
+          funcRet.push(tokens[pos++]);
+          continue;
+        }
+        if (tokens[pos] !== ')' && tokens[pos] !== ';') {
+          funcArgs.push(tokens[pos++]);
+        } else {
+          pos++;
+        }
       }
-      funcName = tokens[pos++];
-      if (tokens[pos] !== '(') {
+      // funcRet.push(tokens[pos++]);
+      // if (tokens[pos] === '*') {
+      //   funcRet.push(tokens[pos++]);
+      // }
+      // funcName = tokens[pos++];
+      // if (tokens[pos] !== '(') {
+      //   badlines.push(line);
+      //   continue;
+      // }
+      // pos++;
+      // while (pos < tokens.length && tokens[pos] !== ')') {
+      //   funcArgs.push(tokens[pos++]);
+      // }
+      if (funcName) {
+        if (!filterFunction(funcName)) {
+          APINames.push(funcName);
+          self.writeToSourceFile(sourceStream, funcRet, funcName, funcArgs);
+        } else {
+          console.log('Exclude the API: ' + funcName);
+        }
+      } else {
         badlines.push(line);
-        continue;
       }
-      pos++;
-      while (pos < tokens.length && tokens[pos] !== ')') {
-        funcArgs.push(tokens[pos++]);
-      }
-      APINames.push(funcName);
-      self.writeToSourceFile(sourceStream, funcRet, funcName, funcArgs);
     }
     if (badlines.length > 0) {
       console.log('Bad functions detected: total ' + badlines.length);
@@ -172,7 +208,209 @@ Generator.prototype = {
   },
 
   writeToSourceFile: function(stream, funcRet, funcName, funcArgs) {
-    console.log('function: ' + funcRet.join(' ') + ' ' + funcName + '(' + funcArgs.join(' ') + ');');
+    //console.log('function: ' + funcRet.join(' ') + ' ' + funcName + '(' + funcArgs.join(' ') + ');');
+    var getNumOfArgs = function(argList) {
+      if (argList.length === 0 || (argList.length === 1 && argList[0] === 'void')) return 0;
+      var num = 1, i, len;
+      for (i = 0, len = argList.length; i < len; i++) {
+        if (argList[i] === ',') num++;
+      }
+      return num;
+    };
+
+    var iterateArgs = function(argList, cb) {
+      // assume each argument is consist of <argType, argName>. It must not miss argName.
+      var pos = 0;
+      var argTypes = [];
+      var argName;
+      var index = 0;
+      while (pos < argList.length) {
+        if (argList[pos] !== ',') {
+          argTypes.push(argList[pos]);
+        } else {
+          argName = argTypes.pop();
+          cb(argTypes.join(' '), argName, index++);
+          argTypes = [];
+        }
+        pos++;
+      }
+      if (argTypes.length > 1) {
+        argName = argTypes.pop();
+        cb(argTypes.join(' '), argName, index);
+      }
+    };
+
+
+    var numOfArgs = getNumOfArgs(funcArgs);
+
+    stream.write([
+'',
+'NAN_METHOD('+funcName+') {',
+'  NanScope();',
+'  if (args.Length() != '+numOfArgs+') {',
+'    NanThrowTypeError("Invalid arguments");',
+'    NanReturnUndefined();',
+'  }',
+''].join('\n'));
+  
+    var argList = [];
+    var comment = '';
+    iterateArgs(funcArgs, function(argType, argName, index) {
+      //console.log('Iterate argument: ' + argType + '--' + argName);
+      if (argType === 'UINT16' || argType === 'UINT32' || argType === 'UINT64' ||
+          argType === 'int' || argType === 'unsigned int' || argType === 'const int') {
+        stream.write([
+'  '+argType+' arg_'+index+' = ('+argType+')args['+index+']->Int32Value();',
+''
+          ].join('\n'));
+      } else if (argType === 'const unsigned char *' || argType === 'unsigned char *' ||
+          argType === 'UINT8 *' || argType === 'const UINT8 *' || argType === 'void *' ||
+          argType === 'const void *' || argType === 'unsigned int *') {
+        stream.write([
+'  Local<Object> arg_obj_'+index+' = args['+index+']->ToObject();',
+'  '+argType+' arg_'+index+' = ('+argType+')(node::Buffer::Data(arg_obj_'+index+'));',
+''
+          ].join('\n'));
+      } else if (argType === 'const unsigned char **' || argType === 'unsigned char **') {
+        stream.write([
+'  Local<Object> arg_obj_'+index+' = args['+index+']->ToObject();',
+'  '+argType+' arg_'+index+' = ('+argType+')(&((unsigned char*)node::Buffer::Data(arg_obj_'+index+')));',
+''
+          ].join('\n'));
+      } else if (argType === 'LZ_Stream1 *') {
+        comment += '// TODO: support LZ_Stream1 * as argument type.\n';
+      } else if (argType === 'MD5_MB_MGR *') {
+        comment += '// TODO: support MD5_MB_MGR * as argument type.\n';
+      } else if (argType === 'MD5_MB_MGR_X8X2 *') {
+        comment += '// TODO: support MD5_MB_MGR_X8X2 * as argument type.\n';
+      } else if (argType === 'JOB_MD5 *') {
+        comment += '// TODO: support JOB_MD5 * as argument type.\n';
+      } else if (argType === 'SHA1_MB_MGR *') {
+        comment += '// TODO: support SHA1_MB_MGR * as argument type.\n';
+      } else if (argType === 'SHA1_MB_MGR_X8 *') {
+        comment += '// TODO: support SHA1_MB_MGR_X8 * as argument type.\n';
+      } else if (argType === 'JOB_SHA1 *') {
+        comment += '// TODO: support JOB_SHA1 * as argument type.\n';
+      } else if (argType === 'SHA256_MB_MGR *') {
+        comment += '// TODO: support SHA256_MB_MGR * as argument type.\n';
+      } else if (argType === 'SHA256_MB_MGR_X8 *') {
+        comment += '// TODO: support SHA256_MB_MGR_X8 * as argument type.\n';
+      } else if (argType === 'JOB_SHA256 *') {
+        comment += '// TODO: support JOB_SHA256 * as argument type.\n';
+      } else if (argType === 'SHA512_MB_MGR *') {
+        comment += '// TODO: support SHA512_MB_MGR * as argument type.\n';
+      } else if (argType === 'SHA512_MB_MGR_X4 *') {
+        comment += '// TODO: support SHA512_MB_MGR_X4 * as argument type.\n';
+      } else if (argType === 'JOB_SHA512 *') {
+        comment += '// TODO: support JOB_SHA512 * as argument type.\n';
+      } else if (argType === 'MD5_HASH_CTX_MGR *') {
+        comment += '// TODO: support MD5_HASH_CTX_MGR * as argument type.\n';
+      } else if (argType === 'MD5_HASH_CTX *') {
+        comment += '// TODO: support MD5_HASH_CTX * as argument type.\n';
+      } else if (argType === 'HASH_CTX_FLAG') {
+        comment += '// TODO: support HASH_CTX_FLAG as argument type.\n';
+      } else if (argType === 'MD5_MB_JOB_MGR *') {
+        comment += '// TODO: support MD5_MB_JOB_MGR * as argument type.\n';
+      } else if (argType === 'MD5_JOB *') {
+        comment += '// TODO: support MD5_JOB * as argument type.\n';
+      } else if (argType === 'SHA1_HASH_CTX_MGR *') {
+        comment += '// TODO: support SHA1_HASH_CTX_MGR * as argument type.\n';
+      } else if (argType === 'SHA1_HASH_CTX *') {
+        comment += '// TODO: support SHA1_HASH_CTX * as argument type.\n';
+      } else if (argType === 'SHA1_MB_JOB_MGR *') {
+        comment += '// TODO: support SHA1_MB_JOB_MGR * as argument type.\n';
+      } else if (argType === 'SHA1_JOB *') {
+        comment += '// TODO: support SHA1_JOB * as argument type.\n';
+      } else if (argType === 'SHA256_HASH_CTX_MGR *') {
+        comment += '// TODO: support SHA256_HASH_CTX_MGR * as argument type.\n';
+      } else if (argType === 'SHA256_HASH_CTX *') {
+        comment += '// TODO: support SHA256_HASH_CTX * as argument type.\n';
+      } else if (argType === 'SHA256_MB_JOB_MGR *') {
+        comment += '// TODO: support SHA256_MB_JOB_MGR * as argument type.\n';
+      } else if (argType === 'SHA256_JOB *') {
+        comment += '// TODO: support SHA256_JOB * as argument type.\n';
+      } else if (argType === 'SHA512_HASH_CTX_MGR *') {
+        comment += '// TODO: support SHA512_HASH_CTX_MGR * as argument type.\n';
+      } else if (argType === 'SHA512_HASH_CTX *') {
+        comment += '// TODO: support SHA512_HASH_CTX * as argument type.\n';
+      } else if (argType === 'SHA512_MB_JOB_MGR *') {
+        comment += '// TODO: support SHA512_MB_JOB_MGR * as argument type.\n';
+      } else if (argType === 'SHA512_JOB *') {
+        comment += '// TODO: support SHA512_JOB * as argument type.\n';
+      } else {
+        console.log('Failed to parse the argument: ' + argType);
+      }
+      argList.push('arg_' + index);
+    });
+
+    stream.write(comment);
+
+    var funcRetStr = funcRet.join(' ');
+
+    var argstr = '';
+    if (argList.length > 1) {
+      var last = argList.pop();
+      argstr = argList.join(', ') + last;
+    } else if (argList.length === 1) {
+      argstr = argList[0];
+    }
+    stream.write([
+'',
+'  ' + funcRetStr + ' result = ' + funcName + '('+argList.join(', ')+');',
+''
+      ].join('\n'));
+
+    comment = '';
+
+    if (funcRetStr === 'UINT16' || funcRetStr === 'UINT32' || 
+        funcRetStr === 'unsigned int' || funcRetStr === 'int') {
+      stream.write([
+'',
+'  Local<Number> result_wrapper = NanNew(result);',
+'  NanReturnValue(result_wrapper);',
+''
+        ].join('\n'));
+    } else if (funcRetStr === 'void') {
+      stream.write([
+'',
+'  NanReturnUndefined();',
+''
+        ].join('\n'));
+    }/* else if (funcRetStr === 'void *') {
+
+    } */else if (funcRetStr === 'JOB_MD5 *') {
+      comment = '//TODO: return object of JOB_MD5 *';
+    } else if (funcRetStr === 'JOB_SHA1 *') {
+      comment = '//TODO: return object of JOB_SHA1 *';
+    } else if (funcRetStr === 'JOB_SHA256 *') {
+      comment = '//TODO: return object of JOB_SHA256 *';
+    } else if (funcRetStr === 'JOB_SHA512 *') {
+      comment = '//TODO: return object of JOB_SHA512 *';
+    } else if (funcRetStr === 'MD5_HASH_CTX *') {
+      comment = '//TODO: return object of MD5_HASH_CTX *';
+    } else if (funcRetStr === 'MD5_JOB *') {
+      comment = '//TODO: return object of MD5_JOB *';
+    } else if (funcRetStr === 'SHA1_HASH_CTX *') {
+      comment = '//TODO: return object of SHA1_HASH_CTX *';
+    } else if (funcRetStr === 'SHA1_JOB *') {
+      comment = '//TODO: return object of SHA1_JOB *';
+    } else if (funcRetStr === 'SHA256_HASH_CTX *') {
+      comment = '//TODO: return object of SHA256_HASH_CTX *';
+    } else if (funcRetStr === 'SHA256_JOB *') {
+      comment = '//TODO: return object of SHA256_JOB *';
+    } else if (funcRetStr === 'SHA512_HASH_CTX *') {
+      comment = '//TODO: return object of SHA512_HASH_CTX *';
+    } else if (funcRetStr === 'SHA512_JOB *') {
+      comment = '//TODO: return object of SHA512_JOB *';
+    } else {
+      console.log('Failed to recognize the return type: ' + funcRetStr);
+    }
+    if (comment.length > 0) {
+      console.log(comment);
+    }
+
+    stream.write(comment + '}\n');
+
   },
 
   writeToHeaderFile: function(filePath, compName) {
